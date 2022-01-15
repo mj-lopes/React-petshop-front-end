c765cc4a9f1afe32ac33353f29328a68343381c6
unknown <marcos_lopes_ribeiro_junior@hotmail.com>
2022-01-15 14:54:25 -0300

    Component titulo refatorado

--- src/components/Titulo/style.js
+++ src/components/Titulo/style.js
@@ -2,20 +2,25 @@ import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
 
 const StyledTypography = styled(Typography)({
+  position: "relative",
+
   fontSize: "clamp(1rem, 10vw, 4rem)",
   color: "#333",
   lineHeight: "1",
-  position: "relative",
   fontFamily: "Sen, Helvetica, Sans-serif",
 
-  ":after": {
+  " :after": {
     content: '""',
+    width: "25px",
+    height: "25px",
+    zIndex: "-1",
+
     position: "absolute",
-    left: "5px",
-    bottom: "-5px",
-    width: "60px",
-    height: "6px",
+    left: "0px",
+    bottom: "5px",
+
     backgroundColor: "#FFC100",
+    borderRadius: "6px",
   },
 });
 
