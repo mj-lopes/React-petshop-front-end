ea60d6e1576930f70d80f26a1af8a3e4be6d3e90
unknown <marcos_lopes_ribeiro_junior@hotmail.com>
2022-01-16 00:26:43 -0300

    Alteração no cardProduto

--- src/components/CardProduto/index.jsx
+++ src/components/CardProduto/index.jsx
@@ -1,4 +1,10 @@
-import { CardContent, CardHeader, Typography } from "@mui/material";
+import {
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 import Product from "../../asserts/Ração_Seca_Nestlé_Purina_Friskies_Frango_para_Gatos_Adultos_3104249-removebg-preview.png";
 import {
   WrapperCartao,
@@ -10,18 +16,22 @@ import {
 const CardProduto = () => {
   return (
     <WrapperCartao raised>
-      <CardHeader action={"☆4.9"} sx={{ margin: "0px" }} />
-      <CardImg image={Product} component={"img"} />
-      <CardContent>
-        <Typography paragraph textAlign={"center"} fontFamily={"Sen"}>
-          Ração asdasd
-        </Typography>
-        <PrecoAVista paragraph>
-          R$ 99,99 <span>ou</span>
-          <br />
-          <PrecoParcelado>até 3x de R$ 35,99</PrecoParcelado>
-        </PrecoAVista>
-      </CardContent>
+      <Link to="/produtos/cachorro">
+        <CardActionArea>
+          <CardHeader action={"☆4.9"} sx={{ margin: "0px" }} />
+          <CardImg image={Product} component={"img"} />
+          <CardContent>
+            <Typography paragraph textAlign={"center"} fontFamily={"Sen"}>
+              Ração asdasd
+            </Typography>
+            <PrecoAVista paragraph>
+              R$ 99,99 <span>ou</span>
+              <br />
+              <PrecoParcelado>até 3x de R$ 35,99</PrecoParcelado>
+            </PrecoAVista>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </WrapperCartao>
   );
 };
