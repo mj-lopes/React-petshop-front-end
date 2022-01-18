2447efd46f4d922b450791dfeaf63855b32655d0
unknown <marcos_lopes_ribeiro_junior@hotmail.com>
2022-01-18 00:21:46 -0300

    Add válidacao do formulario de login

--- src/pages/Autenticacao/Login/index.jsx
+++ src/pages/Autenticacao/Login/index.jsx
@@ -1,24 +1,51 @@
 import { Box } from "@mui/material";
 import { Botao, Subtitulo, Titulo } from "../../../components";
 import Input from "../../../components/Input";
+import { useFormik } from "formik";
 
-const Login = () => (
-  <>
-    <Titulo>Login</Titulo>
-    <form>
-      <Input label="Usuario" />
-      <Input label="Senha" />
-      <Botao amarelo="y">Entrar</Botao>
-    </form>
+const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      usuario: "",
+      senha: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 4));
+    },
+  });
 
-    <Box my={4}>
-      <Subtitulo>Cadastre-se</Subtitulo>
-      <p style={{ marginBottom: "1rem" }}>
-        Ainda não possui conta? Cadastre-se, é seguro e gratuito.
-      </p>
-      <Botao branco="y">Cadastrar</Botao>
-    </Box>
-  </>
-);
+  return (
+    <>
+      <Titulo>Login</Titulo>
+      <form onSubmit={formik.handleSubmit}>
+        <Input
+          label="Usuario"
+          id="usuario"
+          name="usuario"
+          value={formik.values.usuario}
+          onChange={formik.handleChange}
+        />
+        <Input
+          label="Senha"
+          id="senha"
+          name="senha"
+          value={formik.values.senha}
+          onChange={formik.handleChange}
+        />
+        <Botao amarelo="y" type="submit">
+          Entrar
+        </Botao>
+      </form>
+
+      <Box my={4}>
+        <Subtitulo>Cadastre-se</Subtitulo>
+        <p style={{ marginBottom: "1rem" }}>
+          Ainda não possui conta? Cadastre-se, é seguro e gratuito.
+        </p>
+        <Botao branco="y">Cadastrar</Botao>
+      </Box>
+    </>
+  );
+};
 
 export default Login;
